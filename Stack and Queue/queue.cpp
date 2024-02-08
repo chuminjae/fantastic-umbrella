5106c15e1dc4bb781b03e0075b9ebdae5db33cee
Junwon Kim <junwonkim04@outlook.com>
2024-02-08 22:28:36 +0900

    removing implementing part

--- Stack and Queue/queue.cpp
+++ Stack and Queue/queue.cpp
@@ -1,65 +1,6 @@
 //C++에서는 기본적으로 queue 자료구조를 지원한다 -- STLqueue
 //header에 #include <queue> 추가 (STL queue일 경우)
 //queue <데이터 타입> 이름;
-
-//iostream만 사용해서 queue구현하기
-#include<iostream>
-
-using namespace std;
-
-template <typename T>
-
-class queue {
-private:
-	T* arr;								//큐 원소 넣는 배열
-	int q_size;							//큐의 사이즈
-	int frontCursor;					//큐의 front가리키는 커서
-	int rearCursor;						//큐의 back가리키는 커서
-
-public:
-	queue() {							//초기화
-		q_size = 0;
-		arr = new T[10'000];
-		frontCursor = 5'000;
-		rearCursor = 5'000;
-	}
-	~queue() {
-		delete[] arr;
-	}
-
-	void push(T elem){						//원소 넣기
-		arr[rearCursor] = elem;
-		rearCursor++;
-        q_size++;
-
-	}
-	void pop(T elem){						//원소 빼기
-		if (q_size!=0) {						//비어있지 않을 때
-			arr[frontCursor] = 0;
-			frontCursor++;
-            q_size--;
-		}
-		else {
-			cout << "큐가 비어있습니다." << '\n';
-		}
-	}
-	T front(){								//큐의 제일 앞 값(먼저 빠짐)
-		return arr[frontCursor];
-	}
-	T back(){								//큐의 제일 뒷 값
-		return arr[rearCursor-1];
-	}
-	bool empty(){							//큐가 비어있는지 여부
-		if (q_size==0)
-			return true;
-		else return false;
-	}
-	int size(){								//큐의 크기
-		return q_size;
-	}
-};
-
-
 ///// STL Queue
 
 #include <iostream>
--- Stack and Queue/queue.py
+++ Stack and Queue/queue.py
@@ -1,21 +1,6 @@
 # 파이썬에는 큐 모듈 자체가 있어서 queue모듈을 import 해온뒤에 사용할수 있다.
 # Queue 모듈에서는 queue, PriorityQueue (우선순위큐), LifoQueue (스택) 을 지원한다.
-
-
-###Queue 구현
-
-qlist = []
-
-def enqueue(qlist, data): #Queue에 원소 하나 추가
-    qlist.append(data)
-
-def dequeue(qlist): #Queue에서 원소 제거
-    data = qlist[0]
-    del qlist[0]
-    return data
-
 ### Queue() 쓰기
-
 import queue
 
 Q = queue.Queue()
