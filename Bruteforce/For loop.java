e24c6703f9148570ea82d5cc2f54020512b2bc17
c_vgvrthrg <choochootrain1130@gmail.com>
2024-02-09 18:06:08 +0900

    Create For loop.java

---
+++ Bruteforce/For loop.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int cnt = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i + j == 13) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
