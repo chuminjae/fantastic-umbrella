1500addf7cc4b0759fcddb7e68d7be4622dac56a
c_vgvrthrg <choochootrain1130@gmail.com>
2024-02-09 17:37:55 +0900

    Update stack.cpp

--- Stack and Queue/stack.cpp
+++ Stack and Queue/stack.cpp
@@ -2,23 +2,19 @@
 //header에 #include <stack> 추가 (STL stack일 경우)
 //queue <데이터 타입> 이름;
 
-#include <iostream>
-#include <stack>
-
+# include <bits/stdc++.h>
 using namespace std;
-
-int main(int args , char** argv){
+int main(){
     stack<int> s;
-
-    s.push(1);
-    s.push(2);
-    s.push(3);
+    s.push(1); //1
+    s.push(2); // 2 1
+    s.push(3); // 3 2 1 
     cout << "empty(0 is not empty, 1 is empty) : " << s.empty() << endl;
-    cout << "size : " << s.size() << endl;
+    cout << "size : " << s.size() << endl; // stack안에 들어있는 원소의 개수
     cout << "stack values : ";
     while(!s.empty()){
-        cout << s.top() << ' ';
-        s.pop();
+        cout << s.top() << ' '; // 탑은 3 2 1 순으로 바꿈
+        s.pop(); // top에 있는 원소를 제거
     }
     cout << "\n" << "empty(0 is not empty, 1 is empty): " << s.empty() << endl ;    
 }
